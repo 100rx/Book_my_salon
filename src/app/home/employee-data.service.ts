b394dce336a8c8f45ed6e9371a507bf79e14f0ce
Ankur Roy <79366495+ankur-roy-byte@users.noreply.github.com>
2022-07-11 17:55:25 +0530

    some new modifications

--- src/app/home/employee-data.service.ts
+++ src/app/home/employee-data.service.ts
@@ -4,9 +4,11 @@ import {Employee} from './employee-data';
 export class EmployeeData implements InMemoryDbService {
   createDb() {
     let employees : Employee[]=[
-      { id: 1, name: "Ram", location: "Bangalore", email:"ram@mail.com", mobile:"9867512345" },
-     { id: 2, name: "Raj", location: "Chennai", email:"raj@mail.com", mobile:"7867534521" },
-     { id: 3, name: "Vinay", location: "Pune", email:"vinay@mail.com", mobile:"9975287450" }
+      { id: 1, name: "Cute-cut", location: "Mysore", email:"ram@mail.com", mobile:"9867512345" },
+     { id: 2, name: "Hair-with-flair", location: "Chennai", email:"raj@mail.com", mobile:"7867534521" },
+     { id: 5, name: "Florex", location: "Mysore", email:"manju@mail.com", mobile:"6534587654" },
+     { id: 3, name: "Better-feel", location: "Bangalore", email:"ramnath@mail.com", mobile:"9975437459" },
+     { id: 4, name: "Curly-salon", location: "Pune", email:"vinay@mail.com", mobile:"9975287450" }
     ];
     return {employees};
   }
